5dd2a8ff1243f4702ff081a99d5516c87ea22961
Abhay Gautam <68742118+Eshuv@users.noreply.github.com>
2025-01-23 06:38:54 +0000

    Rotated Array

---
+++ SearchInRoatedSortedArray.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Search(vector<int> arr, int key)
+{
+    int lo = 0, hi = arr.size() - 1;
+
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] >= arr[lo]) {
+
+            if (key >= arr[lo] && key < arr[mid])
+                hi = mid - 1;
+
+            else
+                lo = mid + 1;
+        }
+
+        else {
+          
+            if (key > arr[mid] && key <= arr[hi])
+                lo = mid + 1;
+
+            else
+                hi = mid - 1;
+        }
+    }
+
+    return -1; 
+}
+
+int main()
+{
+    vector<int> vec = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+    int key = 3;
+    int res = Search(vec, key);
+
+    cout << res << endl;
+}
